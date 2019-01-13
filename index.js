e253ef276569070927841067d7fff70d9098ee4e
fadi ina <fadiina610@gmail.com>
2019-01-13 13:18:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,39 @@
-// Code your solution in this file!
+// i want to thank every company that learn people coding
+// for free 
+function logDriverNames(drivers){
+  drivers.forEach((driver)=> {
+    console.log(driver.name)
+  })
+}
+
+function logDriversByHometown(drivers, hometown){
+  drivers.forEach((driver)=> {
+    if(driver.hometown == hometown){
+      console.log(driver.name)
+    }
+  })
+}
+
+function driversByRevenue(drivers){
+  let sorted = drivers.sort((driverOne, driverTwo) => {
+    return driverOne.revenue - driverTwo.revenue
+  })
+  return sorted
+}
+
+function driversByName(drivers){
+  let sorted = drivers.sort((driverOne, driverTwo) => {
+    return driverOne.name.localeCompare(driverTwo.name)
+  })
+  return sorted
+}
+
+function totalRevenue(drivers){
+  return drivers.reduce(function(previous, currentDriver){
+    return currentDriver.revenue + previous
+  }, 0)
+}
+
+function averageRevenue(drivers){
+  return totalRevenue(drivers) / drivers.length
+}
